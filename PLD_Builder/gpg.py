b5c2ab689c9f3dfc8b50d07e55285a60b53cd2cc
Michał Moskal <malekith@pld-linux.org>
2003-06-08 13:33:13 +0000

    - work in progress
    Changed files:
        PLD_Builder/gpg.py -> 1.2
        PLD_Builder/request.py -> 1.2
        PLD_Builder/squeue.py -> 1.1
        go.py -> 1.3

--- PLD_Builder/gpg.py
+++ PLD_Builder/gpg.py
@@ -1,21 +1,40 @@
 import popen2
 import re
+import xreadlines
+import StringIO
 
-def verify_sig(email):
+def verify_sig(buf):
   """Check signature.
   
-  Given email as list of strings, return (signer-emails, signed-body).
-  where both are lists of strings.
+  Given email as file-like object, return (signer-emails, signed-body).
+  where signer-emails is lists of strings, and signed-body is StringIO
+  object.
   """
   (gpg_out, gpg_in, gpg_err) = popen2.popen3("LC_ALL=C gpg --decrypt")
-  for l in email:
-    gpg_in.write(l)
+  gpg_in.write(buf.read())
   gpg_in.close()
-  body = gpg_out.readlines()
+  body = StringIO.StringIO()
+  for l in gpg_out.xreadlines():
+    body.write(l)
   rx = re.compile("^gpg: Good signature from .*<([^>]+)>")
   emails = []
-  for l in gpg_err.readlines():
+  for l in gpg_err.xreadlines():
     m = rx.match(l)
     if m:
       emails.append(m.group(1))
+  body.seek(0)
+  gpg_out.close()
+  gpg_err.close()
   return (emails, body)
+
+def sign(buf):
+  (gpg_out, gpg_in, gpg_err) = popen2.popen3("LC_ALL=C gpg --clearsign --default-key builder")
+  gpg_in.write(buf.read())
+  gpg_in.close()
+  body = StringIO.StringIO()
+  for l in gpg_out.xreadlines():
+    body.write(l)
+  body.seek(0)
+  gpg_out.close()
+  gpg_err.close()
+  return body
