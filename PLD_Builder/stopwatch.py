93b720ead5dae4902e44b1588ca76596139e5caf
Michal Moskal <michal@moskal.me>
2003-06-14 15:37:33 +0000

    - resource usage stopwatch
    Changed files:
        PLD_Builder/stopwatch.py -> 1.1

---
+++ PLD_Builder/stopwatch.py
@@ -0,0 +1,43 @@
+import time
+import resource
+
+class Time:
+  def __init__(self):
+    x = resource.getrusage(resource.RUSAGE_CHILDREN)
+    self.user_time = x[0]
+    self.sys_time = x[1]
+    self.non_io_faults = x[6]
+    self.io_faults = x[7]
+    self.time = time.time()
+
+  def sub(self, x):
+    self.user_time -= x.user_time
+    self.sys_time -= x.sys_time
+    self.non_io_faults -= x.non_io_faults
+    self.io_faults -= x.io_faults
+    self.time -= x.time
+
+  def format(self):
+    return "user:%.2fs sys:%.2fs real:%.2fs (faults io:%d non-io:%d)" % \
+                (self.user_time, self.sys_time, self.time, self.io_faults, 
+                 self.non_io_faults)
+    
+class Timer:
+  def __init__(self):
+    self.starts = []
+
+  def start(self):
+    self.starts.append(Time())
+
+  def stop(self):
+    tmp = Time()
+    tmp.sub(self.starts.pop())
+    return tmp.format()
+
+t = Timer()
+
+def start():
+  t.start()
+
+def stop():
+  return t.stop()
