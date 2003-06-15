334c980e12484e8130433c8c601904c2eb8726c7
Michał Moskal <malekith@pld-linux.org>
2003-06-15 14:02:34 +0000

    - allow logging via syslog
    Changed files:
        PLD_Builder/config.py -> 1.14
        PLD_Builder/log.py -> 1.6
        config/builder.conf -> 1.12

--- PLD_Builder/log.py
+++ PLD_Builder/log.py
@@ -1,27 +1,36 @@
 import sys
 import time
+import syslog
 
 import path
 
 builder = ""
+do_syslog = 0
 
-def log(s):
+def log(p, s):
+  if do_syslog:
+    syslog.syslog(p, s)
   f = open(path.log_file, "a")
   f.write("%s [%s]: %s\n" % (time.asctime(), builder, s))
   f.close()
   
+def panic(s):
+  log(syslog.LOG_ALERT, "PANIC: %s" % s)
+  raise "PANIC: %s" % s
+
 def alert(s):
-  log("alert: %s" % s) 
+  log(syslog.LOG_ALERT, "alert: %s" % s) 
  
 def error(s):
-  log("error: %s" % s) 
+  log(syslog.LOG_ERR, "error: %s" % s) 
  
 def warn(s):
-  log("warning: %s" % s) 
+  log(syslog.LOG_WARNING, "warning: %s" % s) 
  
 def notice(s):
-  log("notice: %s" % s) 
+  log(syslog.LOG_NOTICE, "notice: %s" % s) 
 
-def panic(s):
-  log("PANIC: %s" % s)
-  raise "PANIC: %s" % s
+def open_syslog(name, f):
+  global do_syslog
+  do_syslog = 1
+  syslog.openlog(name, syslog.LOG_PID, f)
