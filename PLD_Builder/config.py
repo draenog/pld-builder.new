a50b0d78402c228cf7eb11cd2ad43ae3ba5c911b
Michal Moskal <michal@moskal.me>
2003-06-15 14:02:34 +0000

    - allow logging via syslog
    Changed files:
        PLD_Builder/config.py -> 1.14
        PLD_Builder/log.py -> 1.6
        config/builder.conf -> 1.12

--- PLD_Builder/config.py
+++ PLD_Builder/config.py
@@ -1,10 +1,32 @@
 import ConfigParser
 import string
 import os
+import syslog
 
 import path
 import log
 import status
+
+
+syslog_facilities = {
+  'kern': syslog.LOG_KERN,
+  'user': syslog.LOG_USER,
+  'mail': syslog.LOG_MAIL,
+  'daemon': syslog.LOG_DAEMON,
+  'auth': syslog.LOG_AUTH,
+  'lpr': syslog.LOG_LPR,
+  'news': syslog.LOG_NEWS,
+  'uucp': syslog.LOG_UUCP,
+  'cron': syslog.LOG_CRON,
+  'local0': syslog.LOG_LOCAL0,
+  'local1': syslog.LOG_LOCAL1,
+  'local2': syslog.LOG_LOCAL2,
+  'local3': syslog.LOG_LOCAL3,
+  'local4': syslog.LOG_LOCAL4,
+  'local5': syslog.LOG_LOCAL5,
+  'local6': syslog.LOG_LOCAL6,
+  'local7': syslog.LOG_LOCAL7
+}
 
 class Builder_Conf:
   def __init__(self):
@@ -15,15 +37,23 @@ class Builder_Conf:
     p = ConfigParser.ConfigParser()
     def get(o, d = None):
       if p.has_option(builder, o):
-        return p.get(builder, o)
+        return string.strip(p.get(builder, o))
       elif p.has_option("all", o):
-        return p.get("all", o)
+        return string.strip(p.get("all", o))
       elif d != None:
         return d
       else:
         log.panic("cannot find %s::%s" % (builder, o))
     
     p.readfp(open(path.builder_conf))
+
+    if p.has_option("all", "syslog"):
+      f = p.get("all", "syslog")
+      if f != "":
+        if syslog_facilities.has_key(f):
+          log.open_syslog("builder", syslog_facilities[f])
+        else:
+          log.panic("no such syslog facility: %s" % f)
 
     if builder == "src":
       builder = get("src_builder", builder)
@@ -51,6 +81,14 @@ class Builder_Conf:
     self.job_slots = int(get("job_slots"))
     self.max_load = float(get("max_load"))
     self.control_url = get("control_url")
+    
+    f = get("syslog", "")
+    if f != "":
+      if syslog_facilities.has_key(f):
+        log.open_syslog(self.builder, syslog_facilities[f])
+      else:
+        log.panic("no such syslog facility: %s" % f)
+
     self.done = 1
 
 config = Builder_Conf()
