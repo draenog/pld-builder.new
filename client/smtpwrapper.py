3b6d257d4d79885ea5ddf10ba288056e61f213d2
Mateusz Korniak <matkor@pld-linux.org>
2003-11-07 13:47:03 +0000

    - substite of '/usr/sbin/sendmail -t' sending mail to remote SMTP server. Omta segfaults on Ra,rest of MTA need configuration.
    Changed files:
        client/smtpwrapper.py -> 1.1

---
+++ client/smtpwrapper.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+# As omta segfaults and rest is so huge and does't work out of box
+# mailer="./smtpwrapper.py" or whatever path is
+
+smtp_host = "beauty.ant.vpn"
+
+import smtplib,sys
+
+msg = sys.stdin.read()
+
+server = smtplib.SMTP(smtp_host)
+# server.set_debuglevel(1)
+server.sendmail("matkor@pld-linux.org","builder-ac@pld-linux.org", msg) # Adresses should be taken from .requestrc 
+server.quit()
+
