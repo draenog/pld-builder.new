1081e4e2a78887781f4d1ef1f68547bc2b581c70
Robert Jakub Woźny <speedy@pld-linux.org>
2004-01-25 20:23:52 +0000

    - fix hash (when request is commnand)
    Changed files:
        PLD_Builder/request.py -> 1.25

--- PLD_Builder/request.py
+++ PLD_Builder/request.py
@@ -204,7 +204,11 @@ class Batch:
       link_pre = ""
       link_post = ""
       if bl_archs.has_key(b) and (s == "OK" or s == "FAIL") and len(self.spec) > 5:
-        path = "/%s/%s/%s.bz2" % (b.replace('-','/'), s, self.spec[:len(self.spec)-5])
+        if self.is_command():
+	   bl_name = "command"
+	 else:
+	   bl_name = self.spec[:len(self.spec)-5]
+        path = "/%s/%s/%s.bz2" % (b.replace('-','/'), s, bl_name)
         is_ok = 0
         if s == "OK": is_ok = 1
         link_pre = "<!-- from %s --><a href=\"http://buildlogs.pld-linux.org/index.php?idx=%d&ok=%d&id=%s\">" \
