ce3c967b833617053467d51e0b3ae6feb7e4ae49
Mariusz Mazur <mmazur@pld-linux.org>
2004-09-11 13:56:30 +0000

    - also create and upload *.uploadinfo files that contain list of files in   current batch (these files are uploaded as last in a batch) and the name   of adequate src.rpm - this functionality is required for the new ftp administration scripts, since   they will operate only on metadata placed in such files
    Changed files:
        PLD_Builder/buildlogs.py -> 1.13
        PLD_Builder/file_sender.py -> 1.12
        PLD_Builder/ftp.py -> 1.9
        PLD_Builder/rpm_builder.py -> 1.28

--- PLD_Builder/ftp.py
+++ PLD_Builder/ftp.py
@@ -20,22 +20,26 @@ class FTP_Queue:
 #    else:
     self.url = config.ftp_url
     
-  def add(self, file):
+  def add(self, file, type):
     # if /dev/null, say bye bye
     if self.url == "/dev/null":
       return
     name = os.path.basename(file)
     id = util.uuid()
     shutil.copy(file, path.ftp_queue_dir + id)
-    self.queue.append({'name': name, 'id': id})
+    self.queue.append({'name': name, 'id': id, 'type': type})
     st = os.stat(path.ftp_queue_dir + id)
     self.status += "%10d %s\n" % (st.st_size, name)
 
   def flush(self):
     def desc(l):
-      return "Target: %s/%s\nBuilder: %s\nTime: %d\nRequester: %s\nEND\n" % \
-                (self.url, l['name'], config.builder, time.time(), 
-                 self.requester_email)
+      return """Target: %s/%s
+Builder: %s
+Time: %d
+Type: %s
+Requester: %s
+END
+""" % (self.url, l['name'], config.builder, time.time(), l['type'], self.requester_email)
     
     for l in self.queue:
       f = open(path.ftp_queue_dir + l['id'] + ".desc", "w")
@@ -48,8 +52,8 @@ class FTP_Queue:
 
 queue = FTP_Queue()
 
-def add(f):
-  queue.add(f)
+def add(f, type="rpm"):
+  queue.add(f, type)
 
 def flush():
   queue.flush()
