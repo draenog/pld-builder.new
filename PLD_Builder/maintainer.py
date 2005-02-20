3b081e3526256527fc040f344d895d44eb02aca1
Mariusz Mazur <mmazur@pld-linux.org>
2005-02-20 11:58:13 +0000

    - script for maintaining builders in a good state
    Changed files:
        PLD_Builder/cleaner.py -> 1.2
        PLD_Builder/config.py -> 1.27
        PLD_Builder/maintainer.py -> 1.1
        bin/cleaner.sh -> 1.2
        bin/maintainer.sh -> 1.1

--- PLD_Builder/maintainer.py
+++ PLD_Builder/maintainer.py
@@ -1,16 +1,30 @@
 # vi: encoding=utf-8 ts=8 sts=4 sw=4 et
 
 from config import config, init_conf
+import path
+import os
+import time
+import util
+
+def clean_dir(path, max):
+    curtime=time.time()
+    for i in os.listdir(path):
+        if curtime - os.path.getmtime(path+'/'+i) > max:
+            if os.path.isdir(path+'/'+i):
+                print "rmdir: %s" % path+'/'+i
+            else:
+                print "rmfile: %s" % path+'/'+i
 
 def handle_src():
-    print config.arch
+    clean_dir(path.www_dir+'srpms', 2592000) # a month
 
 def handle_bin():
-    print config.arch
+    pass
 
 if __name__ == '__main__':
     init_conf()
     bb=config.binary_builders[:]
+    clean_dir(path.spool_dir+'builds', config.max_keep_time)
     if config.src_builder:
         try:
             init_conf(config.src_builder)
--- bin/maintainer.sh
+++ bin/maintainer.sh
@@ -3,4 +3,4 @@
 umask 022
 export LC_CTYPE=en_US.iso-8859-1
 cd ~/pld-builder.new
-python PLD_Builder/cleaner.py
+python PLD_Builder/maintainer.py
