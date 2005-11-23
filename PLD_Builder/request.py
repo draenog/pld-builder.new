207659e6b5a28f414027d3d67cb7475683ddb715
ankry <ankry@pld-linux.org>
2005-11-23 18:15:31 +0000

    - of course, I broke it...
    Changed files:
        PLD_Builder/request.py -> 1.35

--- PLD_Builder/request.py
+++ PLD_Builder/request.py
@@ -212,8 +212,8 @@ class Batch:
                     is_ok = 1
                 bld = b.split('-')
                 if bld[0] == "noauto":
-                    bld[1] = bld[2]
                     bld[0] = bld[1]
+                    bld[1] = bld[2]
                 link_pre = "<a href=\"http://buildlogs.pld-linux.org/index.php?dist=%s&arch=%s&ok=%d&id=%s\">" \
                      % (bld[0], bld[1], is_ok, binascii.b2a_hex(md5.new(path).digest()))
                 link_post = "</a>"
