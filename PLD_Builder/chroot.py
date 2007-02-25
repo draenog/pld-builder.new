b06b7a8f7c90d3c6060769448e7a8d3a8c5dd5af
Arkadiusz Miśkiewicz <arekm@pld-linux.org>
2007-02-25 13:23:33 +0000

    Fix order.
    Changed files:
        PLD_Builder/chroot.py -> 1.20
        PLD_Builder/rpm_builder.py -> 1.44

--- PLD_Builder/chroot.py
+++ PLD_Builder/chroot.py
@@ -39,7 +39,7 @@ def run(cmd, user = "builder", logfile = None, logstdout = None):
     else:
         return r
 
-def cp(file, user = "builder", rm=False, outfile):
+def cp(file, outfile, user = "builder", rm=False):
     f = open(outfile, 'w')
     fileno = f.fileno()
     cmd = "cat %s >&%d" % (file, fileno)
