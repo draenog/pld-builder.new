abb5c054f3caec16de9e232096a7b2dc9b5d29f5
Arkadiusz Miśkiewicz <arekm@pld-linux.org>
2007-03-04 12:57:32 +0000

    - limit parallel runs to 1 process
    Changed files:
        bin/request-handler.sh -> 1.4

--- bin/request-handler.sh
+++ bin/request-handler.sh
@@ -2,5 +2,12 @@
 
 umask 022
 export LC_CTYPE=en_US.iso-8859-1
-cd ~/pld-builder.new
-python PLD_Builder/request_handler.py
+
+if lockfile -r3 $HOME/.builder_request_handler.lock 2>/dev/null; then
+	trap "rm -f $HOME/.builder_request_handler.lock" 1 2 3 13 15
+	cd ~/pld-builder.new
+	python PLD_Builder/request_handler.py
+	rm -f $HOME/.builder_request_handler.lock
+else
+	return 1
+fi
