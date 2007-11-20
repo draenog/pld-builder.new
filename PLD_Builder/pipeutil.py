5150d15c7bd6bf00e27635498d753d2038404ba6
Arkadiusz Miśkiewicz <arekm@maven.pl>
2007-11-20 20:56:50 +0000

    Reindent.
    Changed files:
        PLD_Builder/pipeutil.py -> 1.3

--- PLD_Builder/pipeutil.py
+++ PLD_Builder/pipeutil.py
@@ -13,14 +13,19 @@ def rw_pipe(buf_, infd, outfd):
     wr_fin = 0
     buf.seek(pos)
     while not (rd_fin and wr_fin):
-        if wr_fin: o = []
-        else: o = [infd]
-        if rd_fin: i = []
-        else: i = [outfd]
+        if wr_fin:
+            o = []
+        else:
+            o = [infd]
+        if rd_fin:
+            i = []
+        else:
+            i = [outfd]
         i, o, e = select.select(i, o, [])
         if i != []:
             s = os.read(outfd.fileno(), 1000)
-            if s == "": rd_fin = 1
+            if s == "":
+                rd_fin = 1
             ret.write(s)
         if o != []:
             buf.seek(pos)
