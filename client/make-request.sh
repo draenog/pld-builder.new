70d46bcae7550b948da2ac638616935df2255de7
Adam Gołębiowski <adamg@pld-linux.org>
2007-12-11 21:15:22 +0000

    - yet another "fake" distro - ac-xen
    Changed files:
        client/make-request.sh -> 1.40

--- client/make-request.sh
+++ client/make-request.sh
@@ -220,9 +220,14 @@ ac)
 	default_branch="AC-branch"
 	;;
 ac-java) # fake "distro" for java available ac archidectures
-	builder_email="builder-ac@pld-linux.org"
+  builder_email="builder-ac@pld-linux.org"
 	default_builders="ac-i586 ac-i686 ac-athlon ac-amd64"
 	default_branch="AC-branch"
+	;;
+ac-xen) # fake "distro" for xen-enabled architectures
+  builder_email="builder-ac@pld-linux.org"
+  default_builders="ac-i686 ac-athlon ac-amd64"
+  default_branch="AC-branch"
 	;;
 ti)
 	builder_email="builderti@ep09.pld-linux.org"
