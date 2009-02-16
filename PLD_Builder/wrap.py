7653a0079a36720f3156c6727b22b1b1c9f32153
Elan Ruusamäe <glen@pld-linux.org>
2009-02-16 11:21:57 +0000

    - still do use mailer.py (fallback to without) to get proper mail headers set
    Changed files:
        PLD_Builder/wrap.py -> 1.9

--- PLD_Builder/wrap.py
+++ PLD_Builder/wrap.py
@@ -9,22 +9,18 @@ import time
 
 # this module, as it deals with internal error handling shouldn't
 # import anything beside status
-
 import status
 
-def wrap(main):
-    try:
-        main()
-    except:
-        exctype, value = sys.exc_info()[:2]
-        if exctype == SystemExit:
-            sys.exit(value)
-        s = StringIO.StringIO()
-        traceback.print_exc(file = s, limit = 20)
-        log.alert("fatal python exception")
-        log.alert(s.getvalue())
-        log.alert("during: %s" % status.get())
-        
+try:
+    import mailer
+    def sendmail(trace, status):
+        m = mailer.Message()
+        m.set_headers(to = status.admin, cc = ("%s, %s" % status.email, status.builder_list), subject = "fatal python exception")
+        m.write("%s\n" % trace)
+        m.write("during: %s\n" % status)
+        m.send()
+except:
+    def sendmail(trace, status):
         # don't use mailer.py; it safer this way
         f = os.popen("/usr/sbin/sendmail -i -t", "w")
         f.write("""Subject: builder failure
@@ -36,9 +32,25 @@ X-PLD-Builder: fatal error report
 %s
 
 during: %s
-""" % (status.admin, status.email, status.builder_list, 
+""" % (status.admin, status.email, status.builder_list,
              time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()),
-             s.getvalue(), status.get()))
+             trace, status))
         f.close()
+
+def wrap(main):
+    try:
+        main()
+    except:
+        exctype, value = sys.exc_info()[:2]
+        if exctype == SystemExit:
+            sys.exit(value)
+        s = StringIO.StringIO()
+        traceback.print_exc(file = s, limit = 20)
+
+        log.alert("fatal python exception")
+        log.alert(s.getvalue())
+        log.alert("during: %s" % status.get())
+
+        sendmail(s.getvalue(), status.get())
 
         sys.exit(1)
