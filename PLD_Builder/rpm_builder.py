ebf06329defc68936a170a9996a80b3587e3ac8a
Arkadiusz Miśkiewicz <arekm@maven.pl>
2009-09-25 06:57:17 +0000

    - define _specdir and _sourcedir
    Changed files:
        PLD_Builder/rpm_builder.py -> 1.108

--- PLD_Builder/rpm_builder.py
+++ PLD_Builder/rpm_builder.py
@@ -173,8 +173,9 @@ def build_rpm(r, b):
                 max_jobs = max(min(int(os.sysconf('SC_NPROCESSORS_ONLN') * 1.5), config.max_jobs), 1)
                 if r.max_jobs > 0:
                     max_jobs = max(min(config.max_jobs, r.max_jobs), 1)
-                cmd = "cd rpm/SPECS; TMPDIR=%s nice -n %s rpmbuild -bb --define '_smp_mflags -j%d' %s %s" % \
-                            (tmpdir, config.nice, max_jobs, rpmbuild_opt, b.spec)
+                packagedir = "/home/users/builder/rpm/packages/%s" % b.spec[:-5]
+                cmd = "cd rpm/SPECS; TMPDIR=%s nice -n %s rpmbuild -bb --define '_smp_mflags -j%d' --define '_specdir %s' --define '_sourcedir %s' %s %s" % \
+                            (tmpdir, config.nice, max_jobs, packagedir, packagedir, rpmbuild_opt, b.spec)
                 b.log_line("building RPM using: %s" % cmd)
                 begin_time = time.time()
                 res = chroot.run(cmd, logfile = b.logfile)
